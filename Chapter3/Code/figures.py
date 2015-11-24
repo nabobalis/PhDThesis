a4584d08cc70f078d21df8f7a37d0416af971a5c
Nabil Freij <nabil.freij@gmail.com>
2015-11-24 15:45:40 +0000

    Submitted Version

---
+++ Chapter3/Code/figures.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  2 22:35:57 2015
+
+@author: nabobalis
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import astropy.io as io
+
+file_1 = '/data/Data/1999_sunspot.fits' #SVST
+file_2 = '/data/Data/aligned_2005.fits' #DOT 1 
+file_3 = '/data/Data/aligned_2008.fits' # DOT 2 
+
+data_1 = 1
+data_2 = 1
+data_3 = 1
---
+++ README.md
@@ -0,0 +1,7 @@
+PhD Thesis
+==========
+
+My Phd Thesis which is based on the the LaTeX PhD thesis template for Cambridge University Engineering Department.
+[![License MIT](http://img.shields.io/badge/license-MIT-brightgreen.svg)](license.md).
+
+It should build only with PDFLatex.
